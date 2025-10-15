29b379ea655175f2b60008155452b2c2cef31760
sebcoder213 <sebastian.rengifo@my.pbsc.edu>
2025-10-15 11:59:01 -0400

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,1 +1,27 @@
 
+
+
+
+
+
+
+
+
+
+
+
+// e. Add a Value to an Existing Key: Append a new value to an existing key. Done by Samuel Lobl
+//Appending " pie" to the end of "Apple"
+customDictionary[1] += " pie";
+
+foreach (string keyInt in customDictionary.Values)
+{
+    Console.WriteLine(keyInt);
+}
+
+//f.Sort the Keys: Sort the keys in the dictionary. Done by Samuel Lobl
+Console.WriteLine("\nSorted Keys:");
+foreach (int key in customDictionary.Keys.OrderBy(k => k))
+{
+    Console.WriteLine($"{key}: {string.Join(", ", customDictionary[key])}");
+}
