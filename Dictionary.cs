da2786bb6ac82c1d0c666408f7b09df5792dd288
sebcoder213 <sebastian.rengifo@my.pbsc.edu>
2025-10-16 22:00:01 -0400

    Add methods to add and remove custom dictionary keys

--- Dictionary.cs
+++ Dictionary.cs
@@ -4,11 +4,39 @@
 
 
 
+  // Remove an entry from the dictionary -Corrinne Whitt
+    static void RemoveCustomKey()
+    {
+        Console.WriteLine("Enter custom entry to remove.");
+        string keyToRemove = Console.ReadLine();
 
+        if (customDictionary.Remove(keyToRemove))
+        {
+            Console.WriteLine($"Key '{keyToRemove}' removed successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"Key '{keyToRemove}' not found in the dictionary.");
+        }
+    }
+    // Add a new entry to the dictionary -Corrinne Whitt
+    static void AddNewCustomKey()
+    {
+        Console.WriteLine("Enter new custom key:");
+        string newKey = Console.ReadLine();
 
+        if (customDictionary.ContainsKey(newKey))
+        {
+            Console.WriteLine("Key already exists. Try a different key.");
+            return;
+        }
 
+        Console.Write("Enter description for the input: ");
+        string newValue = Console.ReadLine();
 
-
+        customDictionary[newKey] = newValue;
+        Console.WriteLine($"Added key '{newKey}' with value '{newValue}'.");
+    }
 
 // e. Add a Value to an Existing Key: Append a new value to an existing key. Done by Samuel Lobl
 //Appending " pie" to the end of "Apple"
